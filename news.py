8ee3160fd51f90761aaa6ce06bac70f5e84fffb5
minyoung95 <mylim52@naver.com>
2024-10-28 11:02:04 +0900

    Update news.py

--- news.py
+++ news.py
@@ -33,7 +33,7 @@ smtpName = "smtp.naver.com"
 smtpPort = 587
 # 자신의 네이버메일주소,pw, 받는사람이메일주소
 sendEmail = "mylim52@naver.com"
-pw = "dla1954613"
+pw = "9EQ871GFVK3Z"
 recvEmail = "mylim52@naver.com"
 
 title = "랭킹뉴스"
